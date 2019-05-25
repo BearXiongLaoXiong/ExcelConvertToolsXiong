9fb001b014e0fa07fbdea991d57eb07b0a2b9b8b
熊旭 <sh17951@outlook.com>
2019-05-26 03:36:48 +0800

    ui 同步

--- ExcelConvertToolsXiongSetup/Form1.Designer.cs
+++ ExcelConvertToolsXiongSetup/Form1.Designer.cs
@@ -33,7 +33,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
@@ -41,20 +40,22 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(4, 4);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(695, 90);
+            this.button1.Size = new System.Drawing.Size(100, 31);
             this.button1.TabIndex = 0;
             this.button1.Text = "请选择Excel...";
             this.button1.UseVisualStyleBackColor = true;
@@ -62,43 +63,28 @@
             // 
             // button2
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(707, 4);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.Brown;
+            this.button2.Location = new System.Drawing.Point(100, 0);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(695, 90);
+            this.button2.Size = new System.Drawing.Size(101, 31);
             this.button2.TabIndex = 1;
             this.button2.Text = "开始转换";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1406, 98);
-            this.tableLayoutPanel1.TabIndex = 3;
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(61, 6);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dataGridView1.Location = new System.Drawing.Point(32, 3);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(1339, 68);
+            this.dataGridView1.Size = new System.Drawing.Size(650, 45);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
@@ -108,12 +94,11 @@
             dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.MenuBar;
             this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView2.Location = new System.Drawing.Point(61, 86);
-            this.dataGridView2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dataGridView2.Location = new System.Drawing.Point(32, 54);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
             this.dataGridView2.RowTemplate.Height = 23;
-            this.dataGridView2.Size = new System.Drawing.Size(1339, 230);
+            this.dataGridView2.Size = new System.Drawing.Size(650, 114);
             this.dataGridView2.TabIndex = 1;
             // 
             // dataGridView3
@@ -123,12 +108,11 @@
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.MenuBar;
             this.dataGridView3.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView3.Location = new System.Drawing.Point(61, 328);
-            this.dataGridView3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.dataGridView3.Location = new System.Drawing.Point(32, 174);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.ReadOnly = true;
             this.dataGridView3.RowTemplate.Height = 23;
-            this.dataGridView3.Size = new System.Drawing.Size(1339, 474);
+            this.dataGridView3.Size = new System.Drawing.Size(650, 166);
             this.dataGridView3.TabIndex = 2;
             // 
             // tableLayoutPanel2
@@ -143,14 +127,13 @@
             this.tableLayoutPanel2.Controls.Add(this.label2, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.label3, 0, 2);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 98);
-            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 31);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 3;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1406, 808);
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(685, 343);
             this.tableLayoutPanel2.TabIndex = 4;
             // 
             // label1
@@ -158,10 +141,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.label1.Location = new System.Drawing.Point(6, 0);
-            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 54);
+            this.label1.Size = new System.Drawing.Size(18, 32);
             this.label1.TabIndex = 3;
             this.label1.Text = "配\r\n置";
             // 
@@ -170,10 +152,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.label2.Location = new System.Drawing.Point(6, 80);
-            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Location = new System.Drawing.Point(3, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(43, 108);
+            this.label2.Size = new System.Drawing.Size(23, 60);
             this.label2.TabIndex = 4;
             this.label2.Text = "数\r\n据\r\n源";
             // 
@@ -182,31 +163,52 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.label3.Location = new System.Drawing.Point(6, 322);
-            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Location = new System.Drawing.Point(3, 171);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 216);
+            this.label3.Size = new System.Drawing.Size(23, 120);
             this.label3.TabIndex = 5;
             this.label3.Text = "转\r\n换\r\n后\r\n的\r\n结\r\n果";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(685, 31);
+            this.panel1.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.OldLace;
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(201, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(484, 31);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1406, 906);
+            this.ClientSize = new System.Drawing.Size(685, 374);
             this.Controls.Add(this.tableLayoutPanel2);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Excel转换小工具";
-            this.tableLayoutPanel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -215,7 +217,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView3;
@@ -223,6 +224,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- ExcelConvertToolsXiongSetup/Form1.cs
+++ ExcelConvertToolsXiongSetup/Form1.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.IO;
 using System.Windows.Forms;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace ExcelConvertToolsXiongSetup
 {
@@ -10,31 +11,46 @@ namespace ExcelConvertToolsXiongSetup
     {
 
         private string _fileName = "";
+
+        //配置文件Sheet
+        private readonly List<Config> _configList = new List<Config>();
+
         private DataTable _targetTable;
+        private DataTable _dataTable;
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.RowHeadersWidth = 70;
             dataGridView2.RowHeadersWidth = 70;
             dataGridView3.RowHeadersWidth = 70;
             dataGridView1.RowStateChanged += RowStateChanged;
             dataGridView2.RowStateChanged += RowStateChanged;
             dataGridView3.RowStateChanged += RowStateChanged;
+            dataGridView1.CellClick += DataGrid1CellClick;
+            dataGridView2.CellClick += DataGrid2CellClick;
+            dataGridView3.CellClick += DataGrid3CellClick;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
+            _configList.Clear();
             _fileName = "";
+            textBox1.Text = "";
             _targetTable = null;
+            this._dataTable = null;
             dataGridView1.DataSource = null;
             dataGridView2.DataSource = null;
             dataGridView3.DataSource = null;
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                Filter = @"All files (*.*)|*.*|txt files (*.xlsx)|*.xlsx",
+                Filter = @"All files (*.*)|*.*|xlsx(*.xlsx)|*.xlsx",
                 FilterIndex = 2,
                 RestoreDirectory = false
             };
             if (openFileDialog.ShowDialog() == DialogResult.OK) _fileName = openFileDialog.FileName;
+
+            if (_fileName.Length == 0) return;
+            textBox1.Text = _fileName;
 
             string configFile = Environment.CurrentDirectory + "\\列转换配置.xlsx";
             if (!File.Exists(configFile))
@@ -42,15 +58,22 @@ namespace ExcelConvertToolsXiongSetup
                 MessageBox.Show(configFile + @"出现配置文件不存在的致命错误,请恢复配置文件后再操作!\r\n");
                 return;
             }
-
+            //try
+            //{
             DataTable configTable = ExcelOpenXml.GetSheet(configFile, "Sheet1");
+            if (configTable == null || configTable.Rows.Count < 1)
+            {
+                MessageBox.Show("列转换配置表[Sheet1]表数据不完整,请给出正确格式的配置文件");
+                return;
+            }
+            for (int i = 0; i < configTable.Columns.Count; i++)
+                _configList.Add(new Config { Dt1 = configTable.Columns[i].ColumnName.Trim(), Dt2 = configTable.Rows[0][i].ToString().Trim() });
+
             DataTable configMappingTable = ExcelOpenXml.GetSheet(configFile, "Sheet2");
 
-            //配置文件Sheet1 列
-            var cofingList = configTable.Columns.Cast<DataColumn>().Select(x => x.ColumnName.Trim()).ToList();
             //配置文件Sheet2 数据
             var columnsList = configMappingTable.Rows.Cast<DataRow>().Select(x => new { ChargeCurrency = x[0].ToString().Trim(), ChargeCode = x[1].ToString().Trim(), Columns = x[2].ToString().Trim() }).ToList();
-            var cofingListToLower = cofingList.Select(x => x.ToLower());
+            var cofingListToLower = _configList.Select(x => x.Dt1.ToLower());
             var errConfig = columnsList.Where(x => !cofingListToLower.Contains(x.Columns.ToLower())).Select(x => x.Columns);
 
             if (errConfig.Any())
@@ -60,55 +83,56 @@ namespace ExcelConvertToolsXiongSetup
             }
 
 
-            DataTable dataTable = ExcelOpenXml.GetSheet(_fileName, "Sheet0");
-            if (dataTable.Rows.Count < 3)
+            _dataTable = ExcelOpenXml.GetSheet(_fileName, "Sheet0");
+            if (_dataTable.Rows.Count < 3)
             {
                 MessageBox.Show(@"未读取到数据");
                 return;
             }
 
-            for (int i = 0; i < dataTable.Columns.Count; i++)
+            for (int i = 0; i < _dataTable.Columns.Count; i++)
             {
-                string row0 = dataTable.Rows[0][i].ToString().Trim();
-                string row1 = dataTable.Rows[1][i].ToString().Trim();
+                string row0 = _dataTable.Rows[0][i].ToString().Trim();
+                string row1 = _dataTable.Rows[1][i].ToString().Trim();
                 if (row0.Length > 0 && row1.Length == 0)
-                    dataTable.Columns[i].ColumnName = row0;
+                    _dataTable.Columns[i].ColumnName = row0;
                 if (row0.Length > 0 && row1.Length > 0)
-                    dataTable.Columns[i].ColumnName = row1;
+                    _dataTable.Columns[i].ColumnName = row1;
                 if (row0.Length == 0 && row1.Length > 0)
-                    dataTable.Columns[i].ColumnName = row1;
-                dataTable.Columns[i].ColumnName = dataTable.Columns[i].ColumnName.Replace("\r", " ").Replace("\n", " ");
+                    _dataTable.Columns[i].ColumnName = row1;
+                _dataTable.Columns[i].ColumnName = _dataTable.Columns[i].ColumnName.Replace("\r", " ").Replace("\n", " ");
             }
-            dataTable.Rows.RemoveAt(0);
-            dataTable.Rows.RemoveAt(0);
+            _dataTable.Rows.RemoveAt(0);
+            _dataTable.Rows.RemoveAt(0);
+
             _targetTable = new DataTable("Sheet1");
             _targetTable = configTable.Clone();
             _targetTable.TableName = "Sheet1";
-            for (int i = 0; i < dataTable.Rows.Count; i++)
+            for (int i = 0; i < _dataTable.Rows.Count; i++)
             {
                 DataRow row = _targetTable.NewRow();
                 for (int j = 0; j < configTable.Columns.Count; j++)
                 {
-                    for (int k = 0; k < dataTable.Columns.Count; k++)
+                    for (int k = 0; k < _dataTable.Columns.Count; k++)
                     {
-                        if (configTable.Rows[0][j].ToString().Trim().ToLower() == dataTable.Columns[k].ColumnName.Trim().ToLower())
+                        if (_configList[j].Dt2.ToLower() == _dataTable.Columns[k].ColumnName.Trim().ToLower())
                         {
-                            Console.WriteLine(dataTable.Columns[k].ColumnName.Trim().ToLower());
-                            row[j] = dataTable.Rows[i][k];
+                            //Console.WriteLine(dataTable.Columns[k].ColumnName.Trim().ToLower());
+                            row[j] = _dataTable.Rows[i][k];
                         }
                     }
                 }
                 //  Charge Currency	Charge Code 行列转换
-                var targetColumnName = columnsList.FirstOrDefault(x => x.ChargeCurrency.ToLower() == dataTable.Rows[i]["Charge Currency"].ToString().Trim().ToLower() && x.ChargeCode.ToLower() == dataTable.Rows[i]["Charge Code"].ToString().ToLower())?.Columns;
-                if (cofingList.Any(x => x.Contains(targetColumnName ?? "abcdefghigk123")))
-                    row[targetColumnName ?? ""] = dataTable.Rows[i]["Charge Amount"];
+                var targetColumnName = columnsList.FirstOrDefault(x => x.ChargeCurrency.ToLower() == _dataTable.Rows[i]["Charge Currency"].ToString().Trim().ToLower() && x.ChargeCode.ToLower() == _dataTable.Rows[i]["Charge Code"].ToString().ToLower())?.Columns;
+                if (_configList.Select(x => x.Dt1).Any(x => x.Contains(targetColumnName ?? "abcdefghigk123")))
+                    row[targetColumnName ?? ""] = _dataTable.Rows[i]["Charge Amount"];
                 _targetTable.Rows.Add(row);
 
                 //通过BL nr. 列判断 blvposno的计数,从1开始计数，每行++1 直到[BL nr. 列]和上一行不一致后,重新从1开始计数
                 if (cofingListToLower.Contains(@"blvposno") && cofingListToLower.Contains("BL nr.".ToLower()))
                 {
                     int blvposno = 1;
-                    if (i > 0 && i < dataTable.Rows.Count)
+                    if (i > 0 && i < _dataTable.Rows.Count)
                         blvposno = _targetTable.Rows[i]["BL nr."].ToString().ToLower() == _targetTable.Rows[i - 1]["BL nr."].ToString().ToLower()
                                     ? int.Parse(_targetTable.Rows[i - 1]["blvposno"].ToString()) + 1
                                     : 1;
@@ -116,8 +140,14 @@ namespace ExcelConvertToolsXiongSetup
                 }
             }
             dataGridView1.DataSource = configTable;
-            dataGridView2.DataSource = dataTable;
+            dataGridView2.DataSource = _dataTable;
             dataGridView3.DataSource = _targetTable;
+
+            //}
+            //catch (Exception err)
+            //{
+            //    MessageBox.Show(err.Message);
+            //}
         }
 
         private void Button2_Click(object sender, EventArgs e)
@@ -136,5 +166,79 @@ namespace ExcelConvertToolsXiongSetup
         {
             e.Row.HeaderCell.Value = $"{e.Row.Index + 1}";
         }
+
+        private void DataGrid1CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+            var columnName = _configList[e.ColumnIndex].Dt2.Trim().ToLower();
+            var index = dataGridView2.SelectedCells.Count > 0 ? dataGridView2.SelectedCells[0].RowIndex :
+                        dataGridView3.SelectedCells.Count > 0 ? dataGridView3.SelectedCells[0].RowIndex : 0;
+
+            dataGridView3.CurrentCell = dataGridView3.Rows[index].Cells[e.ColumnIndex];
+            for (int i = 0; i < _dataTable.Columns.Count; i++)
+            {
+                if (columnName == _dataTable.Columns[i].ColumnName.Trim().ToLower())
+                {
+                    dataGridView2.CurrentCell = dataGridView2.Rows[index].Cells[i];
+                    return;
+                }
+                else
+                {
+                    dataGridView2.CurrentCell = null;
+                }
+            }
+        }
+
+        private void DataGrid2CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+            var columnName = _dataTable.Columns[e.ColumnIndex].ColumnName;
+
+            for (int i = 0; i < _configList.Count; i++)
+            {
+                if (columnName.Trim().ToLower() == _configList[i].Dt2.Trim().ToLower())
+                {
+                    dataGridView1.CurrentCell = dataGridView1.Rows[0].Cells[i];
+                    dataGridView3.CurrentCell = dataGridView3.Rows[e.RowIndex].Cells[i];
+                    return;
+                }
+                else
+                {
+                    dataGridView1.CurrentCell = null;
+                    dataGridView3.CurrentCell = null;
+                }
+            }
+            //dataGridView1.CurrentCell = dataGridView1.Rows[0].Cells[3];
+            //var list = _configList.Select(x => x.Dt2);
+
+            //dataGridView3.CurrentCell = dataGridView3.Rows[e.RowIndex].Cells[e.ColumnIndex];
+        }
+
+
+        private void DataGrid3CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+            var columnName = _configList[e.ColumnIndex].Dt2.Trim().ToLower();
+
+            dataGridView1.CurrentCell = dataGridView1.Rows[0].Cells[e.ColumnIndex];
+            for (int i = 0; i < _dataTable.Columns.Count; i++)
+            {
+                if (columnName == _dataTable.Columns[i].ColumnName.Trim().ToLower())
+                {
+                    dataGridView2.CurrentCell = dataGridView2.Rows[e.RowIndex].Cells[i];
+                    return;
+                }
+                else
+                {
+                    dataGridView2.CurrentCell = null;
+                }
+            }
+        }
+    }
+
+    public class Config
+    {
+        public string Dt1 { get; set; }
+        public string Dt2 { get; set; }
     }
 }
