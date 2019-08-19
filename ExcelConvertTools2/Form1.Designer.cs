3821f01b05b875461e7b0c9d499212f2a798c0e5
熊旭 <sh17951@outlook.com>
2019-08-19 17:37:13 +0800

    修改BLLD合并

--- ExcelConvertTools2/Form1.Designer.cs
+++ ExcelConvertTools2/Form1.Designer.cs
@@ -37,7 +37,10 @@
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.checkedListBoxPolCode1 = new System.Windows.Forms.CheckedListBox();
             this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.CheckedListBoxCurrency1 = new System.Windows.Forms.CheckedListBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -47,6 +50,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -54,10 +58,10 @@
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Left;
             this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(70, 10);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 62);
+            this.button1.Size = new System.Drawing.Size(200, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "请选择Excel...";
             this.button1.UseVisualStyleBackColor = true;
@@ -68,10 +72,10 @@
             this.button2.Dock = System.Windows.Forms.DockStyle.Left;
             this.button2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.button2.ForeColor = System.Drawing.Color.Brown;
-            this.button2.Location = new System.Drawing.Point(200, 0);
+            this.button2.Location = new System.Drawing.Point(270, 10);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(202, 62);
+            this.button2.Size = new System.Drawing.Size(202, 51);
             this.button2.TabIndex = 1;
             this.button2.Text = "导出结果";
             this.button2.UseVisualStyleBackColor = true;
@@ -83,13 +87,13 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeight = 46;
             this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(61, 84);
+            this.dataGridView1.Location = new System.Drawing.Point(61, 123);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 82;
             this.dataGridView1.RowTemplate.Height = 23;
-            this.dataGridView1.Size = new System.Drawing.Size(1487, 144);
+            this.dataGridView1.Size = new System.Drawing.Size(1504, 141);
             this.dataGridView1.TabIndex = 0;
             // 
             // dataGridView2
@@ -100,13 +104,13 @@
             this.dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView2.ColumnHeadersHeight = 46;
             this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView2.Location = new System.Drawing.Point(61, 240);
+            this.dataGridView2.Location = new System.Drawing.Point(61, 276);
             this.dataGridView2.Margin = new System.Windows.Forms.Padding(6);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
             this.dataGridView2.RowHeadersWidth = 82;
             this.dataGridView2.RowTemplate.Height = 23;
-            this.dataGridView2.Size = new System.Drawing.Size(1487, 222);
+            this.dataGridView2.Size = new System.Drawing.Size(1504, 258);
             this.dataGridView2.TabIndex = 1;
             // 
             // dataGridView3
@@ -117,13 +121,13 @@
             this.dataGridView3.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView3.ColumnHeadersHeight = 46;
             this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView3.Location = new System.Drawing.Point(61, 474);
+            this.dataGridView3.Location = new System.Drawing.Point(61, 546);
             this.dataGridView3.Margin = new System.Windows.Forms.Padding(6);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.ReadOnly = true;
             this.dataGridView3.RowHeadersWidth = 82;
             this.dataGridView3.RowTemplate.Height = 23;
-            this.dataGridView3.Size = new System.Drawing.Size(1487, 303);
+            this.dataGridView3.Size = new System.Drawing.Size(1504, 351);
             this.dataGridView3.TabIndex = 2;
             // 
             // tableLayoutPanel2
@@ -131,7 +135,7 @@
             this.tableLayoutPanel2.ColumnCount = 2;
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Controls.Add(this.checkedListBox1, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel2, 1, 0);
             this.tableLayoutPanel2.Controls.Add(this.dataGridView3, 1, 3);
             this.tableLayoutPanel2.Controls.Add(this.dataGridView2, 1, 2);
             this.tableLayoutPanel2.Controls.Add(this.dataGridView1, 1, 1);
@@ -139,20 +143,50 @@
             this.tableLayoutPanel2.Controls.Add(this.label2, 0, 2);
             this.tableLayoutPanel2.Controls.Add(this.label1, 0, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 62);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 71);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(6);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 4;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(1554, 783);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1571, 903);
             this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkedListBoxPolCode1);
+            this.panel2.Controls.Add(this.checkedListBox1);
+            this.panel2.Controls.Add(this.CheckedListBoxCurrency1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(58, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.panel2.Size = new System.Drawing.Size(1510, 111);
+            this.panel2.TabIndex = 3;
+            // 
+            // checkedListBoxPolCode1
+            // 
+            this.checkedListBoxPolCode1.ColumnWidth = 80;
+            this.checkedListBoxPolCode1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBoxPolCode1.FormattingEnabled = true;
+            this.checkedListBoxPolCode1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.checkedListBoxPolCode1.Location = new System.Drawing.Point(801, 0);
+            this.checkedListBoxPolCode1.MultiColumn = true;
+            this.checkedListBoxPolCode1.Name = "checkedListBoxPolCode1";
+            this.checkedListBoxPolCode1.Size = new System.Drawing.Size(699, 111);
+            this.checkedListBoxPolCode1.TabIndex = 4;
             // 
             // checkedListBox1
             // 
-            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.ColumnWidth = 50;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Items.AddRange(new object[] {
             "1",
@@ -160,18 +194,32 @@
             "3",
             "4",
             "5"});
-            this.checkedListBox1.Location = new System.Drawing.Point(58, 3);
+            this.checkedListBox1.Location = new System.Drawing.Point(278, 0);
             this.checkedListBox1.MultiColumn = true;
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(1493, 72);
+            this.checkedListBox1.Size = new System.Drawing.Size(523, 111);
             this.checkedListBox1.TabIndex = 2;
+            // 
+            // CheckedListBoxCurrency1
+            // 
+            this.CheckedListBoxCurrency1.ColumnWidth = 50;
+            this.CheckedListBoxCurrency1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.CheckedListBoxCurrency1.FormattingEnabled = true;
+            this.CheckedListBoxCurrency1.Items.AddRange(new object[] {
+            "USD",
+            "CNY"});
+            this.CheckedListBoxCurrency1.Location = new System.Drawing.Point(10, 0);
+            this.CheckedListBoxCurrency1.MultiColumn = true;
+            this.CheckedListBoxCurrency1.Name = "CheckedListBoxCurrency1";
+            this.CheckedListBoxCurrency1.Size = new System.Drawing.Size(268, 111);
+            this.CheckedListBoxCurrency1.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.label3.Location = new System.Drawing.Point(6, 468);
+            this.label3.Location = new System.Drawing.Point(6, 540);
             this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 216);
@@ -183,7 +231,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.label2.Location = new System.Drawing.Point(6, 234);
+            this.label2.Location = new System.Drawing.Point(6, 270);
             this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 108);
@@ -195,7 +243,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("微软雅黑", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.MediumSeaGreen;
-            this.label1.Location = new System.Drawing.Point(6, 78);
+            this.label1.Location = new System.Drawing.Point(6, 117);
             this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 54);
@@ -211,25 +259,26 @@
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1554, 62);
+            this.panel1.Padding = new System.Windows.Forms.Padding(70, 10, 10, 10);
+            this.panel1.Size = new System.Drawing.Size(1571, 71);
             this.panel1.TabIndex = 5;
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.OldLace;
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(402, 0);
+            this.textBox1.Location = new System.Drawing.Point(472, 10);
             this.textBox1.Margin = new System.Windows.Forms.Padding(6);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(1152, 62);
+            this.textBox1.Size = new System.Drawing.Size(1089, 51);
             this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1554, 845);
+            this.ClientSize = new System.Drawing.Size(1571, 974);
             this.Controls.Add(this.tableLayoutPanel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -242,6 +291,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.tableLayoutPanel2.ResumeLayout(false);
             this.tableLayoutPanel2.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
@@ -262,6 +312,9 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.CheckedListBox CheckedListBoxCurrency1;
+        private System.Windows.Forms.CheckedListBox checkedListBoxPolCode1;
     }
 }
 
--- ExcelConvertTools2/Form1.cs
+++ ExcelConvertTools2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
@@ -18,6 +19,14 @@ namespace ExcelConvertTools2
         //        只选择Incl.OFT.列的内容为N的数据，其他的数据不要
         //        单独加2列。美金合计 人民币合计
 
+        //增加币种选择
+        //BLLD不再单独作为一行,合并到其他的行中, LDF 为特殊标记
+        //增加HK1,以及和其组合的代码的选项(配置文件)
+
+
+
+
+
         /// <summary>
         /// Excel文件路径
         /// </summary>
@@ -30,13 +39,28 @@ namespace ExcelConvertTools2
         /// 目标表格,要导出的表格
         /// </summary>
         private DataTable _targetTable;
+        private DataTable _configCheckBoxTable;
         private List<ConfigSheet1> _configSheet1List = new List<ConfigSheet1>();
+        /// <summary>
+        /// 币种配置表格
+        /// </summary>
+        private List<string> _configCurrencyCheckedList = new List<string>();
+        /// <summary>
+        /// CollectionOffice配置表格
+        /// </summary>
         private List<string> _configCollectionOfficeCheckedList = new List<string>();
+        /// <summary>
+        /// POD Code配置表格
+        /// </summary>
+        private List<string> _configPolCodeCheckedList = new List<string>();
         public Form1()
         {
             InitializeComponent();
+            CheckedListBoxCurrency1.Items.Clear();
             checkedListBox1.Items.Clear();
+            checkedListBoxPolCode1.Items.Clear();
 
+            dataGridView1.ColumnHeadersHeight = 22;
             dataGridView1.RowHeadersWidth = 70;
             dataGridView2.RowHeadersWidth = 70;
             dataGridView3.RowHeadersWidth = 70;
@@ -44,19 +68,38 @@ namespace ExcelConvertTools2
             dataGridView2.RowStateChanged += RowStateChanged;
             dataGridView3.RowStateChanged += RowStateChanged;
 
+
             //根据collection office的勾选的内容来生成表(需要做出配置功能，只显示常用的几个(大约5个),其他的统统不要)
             string configFile = Environment.CurrentDirectory + "\\Config.xlsx";
-            var configCheckBoxTable = ExcelOpenXml.GetSheet(configFile, "显示collectionoffice集合");
-            if (configCheckBoxTable == null)
+
+            var configChargeCurrency = ExcelOpenXml.GetSheet(configFile, "货币种类");
+            if (configChargeCurrency == null)
+            {
+                MessageBox.Show(@"列转换配置表[Config][货币种类]表数据不完整,请给出正确格式的配置文件");
+                return;
+            }
+            foreach (var item in configChargeCurrency.Columns.Cast<DataColumn>().Select(x => x.ColumnName))
+                CheckedListBoxCurrency1.Items.Add(item, true);
+
+
+            _configCheckBoxTable = ExcelOpenXml.GetSheet(configFile, "显示collectionoffice集合");
+            if (_configCheckBoxTable == null)
             {
                 MessageBox.Show(@"列转换配置表[Config][显示collectionoffice集合]表数据不完整,请给出正确格式的配置文件");
                 return;
             }
 
-            foreach (var item in configCheckBoxTable.Columns.Cast<DataColumn>().Select(x => x.ColumnName))
+            foreach (var item in _configCheckBoxTable.Columns.Cast<DataColumn>().Select(x => x.ColumnName))
             {
                 checkedListBox1.Items.Add(item, true);
+                for (int i = 0; i < _configCheckBoxTable.Rows.Count; i++)
+                {
+                    var value = _configCheckBoxTable.Rows[i][item]?.ToString()?.Trim() ?? "";
+                    if (value.Length > 0) checkedListBoxPolCode1.Items.Add($"{item}-{value}", true);
+                }
             }
+            //todo:event是在改变前，必须选择其他event
+            checkedListBox1.ItemCheck += CheckedListBox1_ItemCheck;
             //checkedListBox1.Items.AddRange(configCheckBoxTable.Columns.Cast<DataColumn>().Select(x => x.ColumnName).ToArray());
         }
 
@@ -65,17 +108,37 @@ namespace ExcelConvertTools2
             _fileName = "";
             textBox1.Text = "";
             _configSheet1List.Clear();
+            _configCurrencyCheckedList.Clear();
             _configCollectionOfficeCheckedList.Clear();
+            _configPolCodeCheckedList.Clear();
             _targetTable = null;
             _dataTable = null;
             dataGridView1.DataSource = null;
             dataGridView2.DataSource = null;
             dataGridView3.DataSource = null;
 
+
+            for (var i = 0; i < CheckedListBoxCurrency1.Items.Count; i++)
+            {
+                var item = CheckedListBoxCurrency1.GetItemChecked(i);
+                if (item) _configCurrencyCheckedList.Add(CheckedListBoxCurrency1.GetItemText(CheckedListBoxCurrency1.Items[i]));
+            }
+            if (_configCurrencyCheckedList.Count == 0)
+            {
+                MessageBox.Show(@"请至少选择一种[货币]");
+                return;
+            }
+
             for (var i = 0; i < checkedListBox1.Items.Count; i++)
             {
                 var item = checkedListBox1.GetItemChecked(i);
                 if (item) _configCollectionOfficeCheckedList.Add(checkedListBox1.GetItemText(checkedListBox1.Items[i]));
+            }
+
+            for (var i = 0; i < checkedListBoxPolCode1.Items.Count; i++)
+            {
+                var item = checkedListBoxPolCode1.GetItemChecked(i);
+                if (item) _configPolCodeCheckedList.Add(checkedListBoxPolCode1.GetItemText(checkedListBoxPolCode1.Items[i]));
             }
 
             var openFileDialog = new OpenFileDialog
@@ -112,10 +175,10 @@ namespace ExcelConvertTools2
 
             //
             //_dataTable = ExcelOpenXml.GetSheet(_fileName, "Sheet0", 3);
-            _dataTable = ExcelOpenXml.GetSheet(_fileName, "Sheet1", 3);
+            _dataTable = ExcelOpenXml.GetSheet(_fileName, "Sheet0", 3);
             if (_dataTable.Rows.Count < 3)
             {
-                MessageBox.Show(@"未读取到数据");
+                MessageBox.Show(@"未读取到[Sheet0]数据");
                 return;
             }
             //Incl. OFT.
@@ -135,14 +198,31 @@ namespace ExcelConvertTools2
             _dataTable.Rows.RemoveAt(0);
 
 
-
+            var collectionOfficeDict = _configCollectionOfficeCheckedList?.ToDictionary(x => x, x => new List<string>());
+            foreach (var item in _configPolCodeCheckedList)
+            {
+                var polCodes = item.Split('-');
+                if (polCodes.Length >= 2 && collectionOfficeDict.ContainsKey(polCodes[0]))
+                    collectionOfficeDict[polCodes[0]].Add(polCodes[1]);
+            }
 
             //只有[Incl. OFT.]值为N才计算,其他值的行,则忽略,仅显示collection office勾选的列
             var table = new DataTable("Sheet0");
             table = _dataTable.Clone();
             for (int i = 0; i < _dataTable.Rows.Count; i++)
-                if (_dataTable.Rows[i]["Incl. OFT."].ToString().Trim().ToUpper() == "N" && _configCollectionOfficeCheckedList.Contains(_dataTable.Rows[i]["Collection Office"].ToString().Trim()))
-                    table.Rows.Add(_dataTable.Rows[i].ItemArray);
+                if (_dataTable.Rows[i]["Incl. OFT."].ToString().Trim().ToUpper() == "N" && _configCollectionOfficeCheckedList.Contains(_dataTable.Rows[i]["Collection Office"].ToString().Trim()) && _configCurrencyCheckedList.Contains(_dataTable.Rows[i]["Charge Currency"].ToString().Trim()))
+                {
+                    var key = _dataTable.Rows[i]["Collection Office"].ToString().Trim();
+                    var polCode = _dataTable.Rows[i]["POL Code"].ToString().Trim();
+                    if (!collectionOfficeDict.ContainsKey(key)) continue;
+                    if (collectionOfficeDict[key].Count > 0)
+                    {
+                        if (collectionOfficeDict[key].Contains(polCode))
+                            table.Rows.Add(_dataTable.Rows[i].ItemArray);
+                    }
+                    else
+                        table.Rows.Add(_dataTable.Rows[i].ItemArray);
+                }
             _dataTable = table;
 
             if (_dataTable.Rows.Count < 1)
@@ -215,7 +295,7 @@ namespace ExcelConvertTools2
                 //检索唯一[BL/No]
                 var rows = table1.Select($"[B/L No.] = '{blNoList[i]}'");
 
-                var datas = rows.Select(x => new
+                var datas = rows.Select(x => new RowModel
                 {
                     Code = x.Field<string>("Charge Code"),
                     Currency = x.Field<string>("Charge Currency"),
@@ -232,7 +312,9 @@ namespace ExcelConvertTools2
                 if (units.Contains("BLLD"))
                 {
                     units.Remove("BLLD");
-                    units.Insert(0, "BLLD");
+                    //units.Insert(0, "BLLD");
+                    foreach (var item in datas.Where(x => x.Unit.Contains("BLLD")))
+                        item.Unit = units[0];
                 }
 
 
@@ -281,6 +363,33 @@ namespace ExcelConvertTools2
         {
             e.Row.HeaderCell.Value = $"{e.Row.Index + 1}";
         }
+
+        private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            checkedListBoxPolCode1.Items.Clear();
+            _configCollectionOfficeCheckedList.Clear();
+            for (var i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                var item = checkedListBox1.GetItemChecked(i);
+                if (item) _configCollectionOfficeCheckedList.Add(checkedListBox1.GetItemText(checkedListBox1.Items[i]));
+            }
+
+            var text = checkedListBox1.GetItemText(checkedListBox1.Items[e.Index]);
+            if (e.NewValue == CheckState.Checked)
+            {
+                if (!_configCollectionOfficeCheckedList.Contains(text)) _configCollectionOfficeCheckedList.Add(text);
+            }
+            else if (_configCollectionOfficeCheckedList.Contains(text)) _configCollectionOfficeCheckedList.Remove(text);
+
+            foreach (var item in _configCollectionOfficeCheckedList)
+            {
+                for (int i = 0; i < _configCheckBoxTable.Rows.Count; i++)
+                {
+                    var value = _configCheckBoxTable.Rows[i][item]?.ToString()?.Trim() ?? "";
+                    if (value.Length > 0) checkedListBoxPolCode1.Items.Add($"{item}-{value}", true);
+                }
+            }
+        }
     }
 
     /// <summary>
@@ -290,5 +399,16 @@ namespace ExcelConvertTools2
     {
         public string ColumnName { get; set; } = string.Empty;
         public bool IsHaveY { get; set; } = false;
+    }
+
+    public class RowModel
+    {
+        public string Code { get; set; }
+        public string Currency { get; set; }
+        public string Rated { get; set; }
+        public string Unit { get; set; }
+        public string Amount { get; set; }
+        public string Us { get; set; }
+        public string Cn { get; set; }
     }
 }
